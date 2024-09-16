81b7c1725c6c2fc2fdce1b799cc0c3868bd23ebf
acamac750 <acamac007@yahoo.com>
2024-09-16 04:52:07 +0000

    CloudKnowledge

--- referenciayoutube.sql
+++ referenciayoutube.sql
@@ -1,3 +1,23 @@
 
 https://www.youtube.com/watch?v=NZYDHhRKoFE
 
+FASTEST Way To Learn Cloud Engineering and ACTUALLY Get a Job
+
+Thanks to DigitalOcean for Sponsoring this video, check out and register for the DigitalOcean Deploy event here on July 9th!
+
+https://do.co/3XSvDQS
+
+FREE CLOUD FOR BEGINNERS GUIDE:
+https://techwithsoleyman.com/
+
+If you want 1 on 1 coaching with me to monetise your tech skill, click below:
+https://noteforms.com/forms/turning-y...
+
+Join My Cloud Engineering Academy:
+https://cloudengineeracademy.io
+
+My Website:
+https://www.soleyman.io/
+
+Business contact:
+hello@soleymanshahir.com
